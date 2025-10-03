b51fe58f5855c4d49e8ab2022a3b21fb06d13c54
adrianlukasikts <adrian.lukasik@technischools.com>
2025-10-03 13:58:48 +0200

    dodano stan należności

--- main.py
+++ main.py
@@ -1,70 +1,131 @@
 import sqlite3
-import datetime
+from datetime import datetime
+
 con = sqlite3.connect("library.db")
 cur = con.cursor()
 
+
+def now():
+    return datetime.today().date()
+
+
 def days_between(d1, d2):
-    d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
-    d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
+    d1 = datetime.strptime(str(d1), "%Y-%m-%d")
+    d2 = datetime.strptime(str(d2), "%Y-%m-%d")
     return abs((d2 - d1).days)
 
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS books (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    title text,
-    author text,
-    year int
-    )
-""")
 
 cur.execute("""
-    CREATE TABLE  IF NOT EXISTS users (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    first_name text,
-    last_name text,
-    email text
-    )
-""")
+            CREATE TABLE IF NOT EXISTS books
+            (
+                id
+                integer
+                PRIMARY
+                KEY
+                AUTOINCREMENT,
+                title
+                text,
+                author
+                text,
+                year
+                int
+            )
+            """)
 
 cur.execute("""
-    CREATE TABLE  IF NOT EXISTS rented_books (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    book_id int,
-    user_id int,
-    rented_date date,
-    finish_date date,
-    FOREIGN KEY(book_id) REFERENCES books(id),
-    FOREIGN KEY(user_id) REFERENCES users(id)
-    ) 
-""")
+            CREATE TABLE IF NOT EXISTS users
+            (
+                id
+                integer
+                PRIMARY
+                KEY
+                AUTOINCREMENT,
+                first_name
+                text,
+                last_name
+                text,
+                email
+                text
+            )
+            """)
 
 cur.execute("""
-    CREATE TABLE  IF NOT EXISTS fees (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    user_id int,
-    fee int,
-    add_date date,
-    FOREIGN KEY(user_id) REFERENCES users(id)
-    ) 
-""")
+            CREATE TABLE IF NOT EXISTS rented_books
+            (
+                id
+                integer
+                PRIMARY
+                KEY
+                AUTOINCREMENT,
+                book_id
+                int,
+                user_id
+                int,
+                rented_date
+                date,
+                finish_date
+                date,
+                FOREIGN
+                KEY
+            (
+                book_id
+            ) REFERENCES books
+            (
+                id
+            ),
+                FOREIGN KEY
+            (
+                user_id
+            ) REFERENCES users
+            (
+                id
+            )
+                )
+            """)
 
-def add_book(title, author, year ):
-    cur.execute("INSERT INTO books(title, author, year) VALUES (?,?,?)" , (title, author, year))
+cur.execute("""
+            CREATE TABLE IF NOT EXISTS fees
+            (
+                id
+                integer
+                PRIMARY
+                KEY
+                AUTOINCREMENT,
+                user_id
+                int,
+                fee
+                int,
+                add_date
+                date,
+                FOREIGN
+                KEY
+            (
+                user_id
+            ) REFERENCES users
+            (
+                id
+            )
+                )
+            """)
+
+
+def add_book(title, author, year):
+    cur.execute("INSERT INTO books(title, author, year) VALUES (?,?,?)", (title, author, year))
     con.commit()
 
 
-
-def add_user(first_name,last_name,email):
-    cur.execute("INSERT INTO users (first_name,last_name,email) VALUES (?,?,?)",(first_name,last_name,email))
+def add_user(first_name, last_name, email):
+    cur.execute("INSERT INTO users (first_name,last_name,email) VALUES (?,?,?)", (first_name, last_name, email))
     con.commit()
+
 
 is_finished = False
-print(days_between(str(datetime.datetime.now()).split()[0], str(datetime.datetime.now()).split()[0]))
 while not is_finished:
     print("1. Dodaj książkę")
     print("2. Dodaj użytkownika")
     print("3. Wypożycz książkę")
     print("4. Zwróć książkę")
+    print("5. Stan Należności")
     print("@. Zakończ program")
     match input():
         case "1":
@@ -72,32 +133,32 @@ while not is_finished:
             title = input("title:")
             author = input("author:")
             year = input("year:")
-            add_book(title,author,year)
+            add_book(title, author, year)
         case "2":
             print("Dodaje użytkownika")
             first_name = input("First name:")
             last_name = input("Last name:")
             email = input("Email:")
-            add_user(first_name,last_name,email)
+            add_user(first_name, last_name, email)
         case "3":
             print("Wypożycz książkę")
             book_id = input("Podaj id książki: ")
-            result = cur.execute("SELECT * FROM books WHERE id=?",(book_id,))
+            result = cur.execute("SELECT * FROM books WHERE id=?", (book_id,))
             if result.fetchone() is None:
                 print("Brak książki")
                 break
-            result2 = cur.execute("SELECT * FROM rented_books WHERE finish_date IS NULL AND book_id = ?",(book_id,))
+            result2 = cur.execute("SELECT * FROM rented_books WHERE finish_date IS NULL AND book_id = ?", (book_id,))
             if result2.fetchone() is not None:
                 print("Ksiazka jest wypozyczona")
                 break
             user_id = input("Podaj id użytkownika: ")
-            result3 = cur.execute("SELECT * FROM users WHERE id=?",[user_id])
+            result3 = cur.execute("SELECT * FROM users WHERE id=?", [user_id])
             if result3.fetchone() is None:
                 print("Nie ma takiego uzytkownika: ")
                 break
-            cur.execute("INSERT INTO rented_books (book_id,user_id,rented_date) VALUES(?,?,?)",(book_id,user_id,datetime.datetime.now()))
+            cur.execute("INSERT INTO rented_books (book_id,user_id,rented_date) VALUES(?,?,?)",
+                        (book_id, user_id, now()))
             con.commit()
-
 
         case "4":
             book_id = input("Id książki")
@@ -110,20 +171,31 @@ while not is_finished:
             if book_info is None:
                 print("Ksiazka jest niewypozyczona")
                 break
-            user_id = input("Podaj id: ")
+            user_id = input("Podaj użytkownika id: ")
             result3 = cur.execute("SELECT * FROM users WHERE id=?", [user_id])
             if result3.fetchone() is None:
                 print("Nie ma takiego uzytkownika: ")
                 break
 
             # book_info[3]
-            days_diff = days_between(str(datetime.datetime.now()).split()[0], str(book_info[3]).split()[0])
+            days_diff = days_between(now(), str(book_info[3]).split()[0])
             if days_diff > 30:
-                cur.execute("INSERT INTO fees(user_id, fee, add_date) VALUES(?,?,?)", (user_id, days_diff - 30,datetime.datetime.now()))
+                cur.execute("INSERT INTO fees(user_id, fee, add_date) VALUES(?,?,?)",
+                            (user_id, days_diff - 30, now()))
 
             cur.execute("UPDATE rented_books SET finish_date = ? WHERE user_id = ? AND finish_date IS NULL",
-                        (str(datetime.datetime.now()).split()[0],user_id))
+                        (now(), user_id))
             con.commit()
+
+        case "5":
+            print("Stan Należności")
+            user_id = input("Podaj użytkownika id: ")
+            result4 = cur.execute("SELECT * FROM users WHERE id=?", [user_id])
+            if result4.fetchone() is None:
+                print("Nie ma takiego uzytkownika: ")
+                break
+            result5 = cur.execute("SELECT SUM(fee) FROM fees WHERE user_id=?", [user_id])
+            print(result5.fetchone()[0])
 
 
         case "@":
